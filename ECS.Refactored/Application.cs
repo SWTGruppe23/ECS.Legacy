7351b3d1103f286eb888202d3a1064658bae932e
andersKallesoee <anderskallesoee@gmail.com>
2021-02-17 09:28:45 +0100

    Added project ECS.Refactored

---
+++ ECS.Refactored/Application.cs
@@ -0,0 +1,18 @@
+﻿namespace ECS.Legacy
+{
+    public class Application
+    {
+        public static void Main(string[] args)
+        {
+            var ecs = new ECS(28);
+
+            ecs.Regulate();
+
+            ecs.SetThreshold(20);
+
+            ecs.Regulate();
+
+            // init commit test
+        }
+    }
+}
